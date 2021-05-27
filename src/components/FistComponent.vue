4f7f1eb499347b856c3fd751107d60142ed6798b
ufochuxian <chen853409335@gmail.com>
2021-05-27 15:27:47 +0800

    自定义组件，并且注册路由，以及绑定data数据中的属性

---
+++ src/components/FistComponent.vue
@@ -0,0 +1,21 @@
+<template>
+
+  <div>
+    <h1 v-text="title">vue的第一个组件</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FistComponent',
+  data () {
+    return {
+      title: 'ToDoList'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
